1871a5ce3f453504530b59fd138f8ad91c6eaeda
justmartin <justin.lamar.martin@gmail.com>
2016-09-20 20:43:26 -0700

    Added new audio file. Added functionality for play, next, and previous buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,39 @@
 
 $(document).ready(function() {
 
-	//loads first song into array on page load
-	song1 = new Song("assets/audio/i-got-you.mp3")
-	Jukebox.add_song(song1);
+	//defines new Jukebox variable
+    jukebox = new Jukebox();
+
+   	//defines new songs and adds them to songs array via Jukebox functions
+	var song1 = new Song("assets/audio/Hall-&-Oates-Sara-Smile.mp3");
+	var song2 = new Song("assets/audio/The-Commodores-Easy.mp3")
+	jukebox.addSong(song1);
+	jukebox.addSong(song2);
+
+	//call Jukebox functions based on corresponding button click using jQuery
+	$("#play-button").click(function() {
+		jukebox.playSong();
+	});
+
+	$("#stop-button").click(function() {
+		jukebox.stopSong();
+	});
+
+	$("#next-button").click(function() {
+		jukebox.nextSong();
+	});
+
+	$("#back-button").click(function() {
+		jukebox.previousSong();
+	});
+
+	$("#volume-down").click(function() {
+		jukebox.turnVolumeDown();
+	});
+
+	$("#volume-up").click(function() {
+		jukebox.turnVolumeUp();
+	});
 
 });
 
@@ -26,35 +56,43 @@ function Song(source) {
 	this.source = source;
 };
 
-//creates Jukebox object to store songs in blank array. contains functions to add/play/sift through songs array
-var Jukebox = {
-	songs: [],
+//defines Jukebox object
+function Jukebox() {
+	i = 0; //defines interator
+	this.songs = []; //defines array to store song sources
 
-	add_song: function(song) {
-		this.songs.push(song);
-	},
+	this.addSong = function(song) {
+		this.songs.push(song); //adds sources to songs array
+		$("#audio-player").attr("src", this.songs[0].source); //assigns first song in array to audio source on load 
+	};
 
-	play_song: function() {
-		$("#audio-player").attr("src", "assets/audio/i-got-you.mp3" /*Need to insert array locations here instead*/);
-		document.getElementById("audio-player").play();
-	},
+	this.playSong = function() {
+		$("#audio-player")[0].play(); //plays current indexed song
+	};
 
-	stop_song: function() {
-		document.getElementById("audio-player").pause();
-	},
+	this.stopSong = function() {
+		$("#audio-player")[0].pause(); //stops current indexed song
+	};
 
-	turn_volume_down: function() {
-		document.getElementById("audio-player").volume-=0.1;
-	},
+	this.nextSong = function(songs) {
+		i = i + 1; //iterates to next song in array
 
-	turn_volume_up: function() {
-		document.getElementById('audio-player').volume+=0.1;
-	},
+		$("#audio-player")[0].src = this.songs[i].source; //assigns source of audio tag based on array index
+		this.playSong(); //plays current indexed song
+	};
+
+	this.previousSong = function() {
+		i = i - 1; //iterates to previous song in array
+
+		$("#audio-player")[0].src = this.songs[i].source;
+		this.playSong();
+	};
+
+	this.turnVolumeDown = function() {
+		$("#audio-player")[0].volume-=0.1; //decreases volume
+	};
+
+	this.turnVolumeUp = function() {
+		$("#audio-player")[0].volume+=0.1; //increases volume
+	};
 };
-
-//call Jukebox functions based on corresponding button click
-document.getElementById("play-button").onclick = Jukebox.play_song;
-document.getElementById("stop-button").onclick = Jukebox.stop_song;
-document.getElementById("volume-down").onclick = Jukebox.turn_volume_down;
-document.getElementById("volume-up").onclick = Jukebox.turn_volume_up;
-
